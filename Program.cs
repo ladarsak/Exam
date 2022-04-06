fcb49da10af0f9135c16b4efc70b23a216247914
Shulga <ladarsak@gmail.com>
2022-04-06 13:45:00 +0300

    [initial commit]

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// Первый вариант программы (длинный, некрасивый, рабочий)
+
+string[] initialArray = new string[] { "John", "Ann", "Li", "Chan", "Richard" };
+
+int resultArrayLength = 0;
+int arrayCount = 0;
+
+for (int i = 0; i < initialArray.Length; i++)
+{
+    if (initialArray[i].Length < 4)
+    {
+        resultArrayLength++;
+    }
+}
+
+string[] resultArray = new string[resultArrayLength];
+for (int i = 0; i < initialArray.Length; i++)
+{
+    if (initialArray[i].Length < 4)
+    {
+        resultArray[arrayCount] = initialArray[i];
+        Console.WriteLine(resultArray[arrayCount]);
+        arrayCount++;
+    }
+}
+
+// Второй вариант программы (короткий, красивый, рабочий.
+// Однако наполовину - копипаста из справочника по С#, поэтому закомменченный)
+
+/*
+string[] initialArray = new string[] { "John", "Ann", "Li", "Chan", "Richard" };
+string[] resultArray = Array.FindAll(initialArray, names => names.Length < 4);
+for (int i = 0; i < resultArray.Length; i++)
+{
+    Console.WriteLine(resultArray[i]);
+}
+*/
